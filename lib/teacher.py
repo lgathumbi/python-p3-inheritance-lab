590e3e8fc8d43555960facd18b0e630f0bd81ff7
lorna gathumbi <nyawiralornah@gmail.com>
2024-12-03 13:35:35 +0300

    inheritance lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,14 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Basic Teaching Skills"]
 
     def teach(self):
+        if len(self.knowledge) > 0:
+           random_index = random.randint(0, len(self.knowledge) - 1)
+           return self.knowledge[random_index]
+        else:
+             return "No knowledge available to teach."
         pass
